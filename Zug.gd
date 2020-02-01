3f520b7d4a1887aa312be086cc95cc1e6443e8bf
Poodolf <jianan@web.de>
2020-02-01 17:01:07 +0100

    Path

---
+++ Zug.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var speed = 100
+onready var parent = get_parent()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	if parent is PathFollow2D:
+		parent.set_offset(parent.get_offset() + speed * delta)
+	pass
+	
