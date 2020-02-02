9580efe25d6792e39567ed3e0ef514aee0c1d414
Marcus Schilling <marcus@5-schillinge.de>
2020-02-02 16:40:18 +0100

    delete child correct only one child is deleted for a fix

--- GUI.gd
+++ GUI.gd
@@ -20,8 +20,11 @@ func howManyLeft():
 	return 1 - scraps.size()
 
 func repair():
-	scraps.clear()
-	delete_children(get_node("HBoxContainer"))
+	scraps.remove(0)
+	var parent = get_node("HBoxContainer")
+	var child = parent.get_child(0)
+	parent.remove_child(child)
+	child.free()
 	
 func delete_children(node):
 	for n in node.get_children():
