acf252b3a5b1f2c468520f3a6da3883df233ae9c
Poodolf <jianan@web.de>
2020-02-02 15:38:33 +0100

    direction shit node kacke

--- RemoteTransform2D.gd
+++ RemoteTransform2D.gd
@@ -1,8 +1,9 @@
 extends RemoteTransform2D
 
 var speed = 100
+var old = Vector2(0,0)
+var motion = Vector2(0,0)
 onready var parent = get_parent()
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,3 +14,9 @@ func _process(delta):
 		parent.set_offset(parent.get_offset() + speed * delta)
 	pass
 	#print(str(global_position.x) + " " + str(global_position.y))
+	motion = Vector2(global_position.x - old.x, global_position.y - old.y)
+	old = Vector2(global_position.x, global_position.y)
+	print(str(motion.x) + " " + str(motion.y))
+	get_node("/root/main/YSort/Floor/Zug").animation(motion)
+	
+	
