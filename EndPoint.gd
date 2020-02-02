ab0a5b9e7a407200c14f809a263b868cc2f061dc
Norbert <norbertkozsir@gmail.com>
2020-02-02 13:33:32 +0100

    implement end game

---
+++ EndPoint.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
+
+
+func _on_Area2D_body_entered(body):
+    if body.get_name() == "Zug":
+        var endMenue = load("res://EndMenue.tscn")
+        get_tree().get_root().add_child(endMenue.instance())
+        get_node("/root/main").queue_free()
+
