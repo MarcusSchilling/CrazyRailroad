89012b26368c84432ce93cc4c18ba1607f1f21b6
Marcus Schilling <marcus@5-schillinge.de>
2020-03-13 14:56:30 +0100

    items are now placed inside the field borders don't crash with route. Requires one fix for borders

--- draw_rails.gd
+++ draw_rails.gd
@@ -18,6 +18,8 @@ var k = 4
 var j = 0
 var path
 var curve
+var places_for_items = {}
+var one_direction_distance = 4
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -33,35 +35,43 @@ func _ready():
 	path.set_curve(curve)
 	var width =15
 	var height = 12
-	var n_tiles = 500
+	var n_tiles = 512
 	var last_tile = Rail.forward
+	var number_of_barriers = 0
 	for s in range(n_tiles):
 		curve = path.get_curve()
 		curve.add_point(map.map_to_world(Vector2(k,j)))
 		map.set_cell(k,j,last_tile)	  
-		createEnvironment(k,j)
 		if (s == n_tiles-1):
-			createEndBarrier(k,j)	
+			createEndBarrier(k,j)
 		if (s % 20 == 0 && s != 0 && s != n_tiles -1):
 			createBarrier(k,j)
+			number_of_barriers = number_of_barriers + 1	
 		if (last_tile == Rail.forward):
+			createEnvironment(Vector2(k,j), +1)
+			createEnvironment(Vector2(k,j), -1)
 			j = j - 1
 		elif(last_tile == Rail.left_forward || last_tile == Rail.right_forward):
+			if(last_tile == Rail.left_forward):
+				createEnvironment(Vector2(k,j), +1)
+			else:
+				createEnvironment(Vector2(k,j), -1)
 			j = j - 1
 		elif(last_tile == Rail.left_right):
 			k = k + k_direction
 		elif(last_tile == Rail.back_left ):
+			createEnvironment(Vector2(k,j), +1)
 			k = k - 1
 		elif(last_tile == Rail.back_right):
+			createEnvironment(Vector2(k,j), -1)
 			k = k + 1
 		else:
 			print("fail")
 		last_tile = nextTile(last_tile)
 	pass	
+	createItems(number_of_barriers + 5)
 	
-	
-var k_new
-var j_new
+var j_old = 0
  # Replace with function body.
 func createBarrier(k,j):
 	var scene = load("res://Obstacle.tscn")
@@ -69,14 +79,20 @@ func createBarrier(k,j):
 	scene_instance.set_name("end")
 	scene_instance.position = map.map_to_world(Vector2(k,j)) + Vector2(0,5)
 	add_child(scene_instance)
-
-	for i in range(1):
+	
+func createItems(number_of_items):
+	for i in range(0, number_of_items):
+		print("item")
 		var tools = load("res://tools.tscn")
 		var tools_scene = tools.instance()
-		k_new = k + (20 - randi() % 40)
-		j_new = j + (randi() % 10)
+		j_old = 0
+		var j_new  = places_for_items.keys()[randi() % places_for_items.size()]
+		var range_places = range(places_for_items.get(j_new).x, places_for_items.get(j_new).y)
+		var k_new = range_places[randi() % range_places.size()]
 		tools_scene.position = map.map_to_world(Vector2(k_new,j_new)) + Vector2(0,5)
-		
+		if (map.get_cell(k_new,j_new) == -1):
+			print("Fail no underground")
+			print("K:", k_new, " J:", j_new)
 		add_child(tools_scene)
 
 func createEndBarrier(k,j):
@@ -85,16 +101,34 @@ func createEndBarrier(k,j):
 	scene_instance.set_name("end")
 	scene_instance.position = map.map_to_world(Vector2(k,j))
 	add_child(scene_instance)
-		
 
 func createEnvironment(k, j):
-	for i in range(20):			
-		if(map.get_cell(k+i,j) == -1):
-			map.set_cell(k + i, j, 6)
-		if(map.get_cell(k-i,j) == -1):
-			map.set_cell(k - i,j,6)
-	map.set_cell(k+20,j,8)
-	map.set_cell(k-20,j,8)
+	var start;
+	var end;
+	for i in range(one_direction_distance):
+		if(map.get_cell(k.x+(j*i),k.y) == -1):
+			if start == null:
+				start = k.x + (j*i)
+			else:
+				end = k.x + (j*i)
+			map.set_cell(k.x + (j*i), k.y, 6)
+		#builds border for the last row that is close to this if the current grass exceeds the last
+		if(map.get_cell(k.x+(j*i),k.y+1) == -1 && k.y != 0):
+			map.set_cell(k.x+(j*i),k.y+1,8)
+	
+	#places for items
+	var vec = Vector2(min(start,end), max(start,end))
+	var a = places_for_items.get(k.y, Vector2(start,end))
+	places_for_items[k.y] = Vector2(min(a.x,vec.x),max(a.y,vec.y))
+	
+	#border
+	map.set_cell(k.x+ (j*one_direction_distance),k.y,8)
+	if(map.get_cell(k.x+ (j*one_direction_distance),k.y+1) == -1):
+		map.set_cell(k.x + (j*one_direction_distance),k.y +1, 8)
+	var i = 0
+	while(map.get_cell(k.x+(j*(one_direction_distance+i)),k.y+1) == 6):
+		map.set_cell(k.x+(j*(one_direction_distance+i)),k.y, 8)
+		i=i+1
 var direction
 func nextTile(tile):
 	match tile:
